2ee74f4a1edaa5dc5ad67ec864b35be62c74fbf7
Morgane Prime <morganeprime@yahoo.com>
2017-03-15 10:30:08 +0100

    Update War.java

--- src/core/War.java
+++ src/core/War.java
@@ -5,6 +5,7 @@ import data.Country;
 public class War {
 	private Country atkCountry;
 	private Country defCountry;
+	//We need to differantiate the atacking army and the defending Country
 	
 	public War(Country atkCountry, Country defCountry) {
 		super();
@@ -12,9 +13,25 @@ public class War {
 		this.defCountry = defCountry;
 	}
 	//to be continued: supposed to deal with fight between two empires
-	public void battle() {
-		if () {
-			
+	//Set Counties and armies
+	public void battle(Country defending, Empire armyBelongs, Empire beingAttacked) {
+		if (atkCountry<defCountry) { 
+			armyBelongs[nomberOfCountriesArmyBelongs+1] = defending;
+			beingAttacked[nomberOfCountriesBeingAttacked] = NULL; 
+			nomberOfCountriesBeingAttacked=nomberOfCountriesBeingAttacked-1;
+			if (nomberOfCountriesBeingAttacked==0){
+				System.out.println("Defending Player LOST");
+			}
+		}
+		else if (atkCountry>defCountry) {
+			atkCountry = NULL;
+			System.out.println("Attacking Army is DESTROYED!");
+		}
+		else{
+			atkCountry = NULL;
+			defCountry = NULL;
+			System.out.println("Defending Army is DESTROYED!");
+			System.out.println("Attacking Army is DESTROYED!");
 		}
 	}
 }
