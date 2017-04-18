4d774958bb9d90bcffecd6f94eeb89a0dba7af60
Morgane Prime <morganeprime@yahoo.com>
2017-04-18 11:24:22 +0200

    Create BuyCountry.java

---
+++ src/core/BuyCountry.java
@@ -0,0 +1,19 @@
+package core;
+
+import java.util.Scanner;
+
+import data.Country;
+
+public class BuyCountry {
+
+public void addCountryNeutural(Empire empire, Country country, Country neutral) {
+		costCountryNeutral=50000000000000000000000000000*country.getNumberCases();
+    if (country.getGold() > costCountryNeutral) {
+			country.setGold(country.getGold() - costCountryNeutral;
+      neutral.seNumberCases(country.getNumberCases()+1);
+		}else{
+			System.out.println("You do not have enough GOLD");
+		}
+}
+
+}
